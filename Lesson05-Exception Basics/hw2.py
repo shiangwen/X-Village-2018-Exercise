b511b14426fd8fb7399c05d8eca3ac1a7f7b4ea4
shiangwen <40990722+shiangwen@users.noreply.github.com>
2018-07-17 23:19:04 +0800

    Delete hw2.py

--- Lesson05-Exception Basics/hw2.py
+++
@@ -1,8 +0,0 @@
-def div(dividend, divisor):
-    print("The answer is {}".format(dividend/divisor))
-
-for i in range(5, -1, -1):
-    for j in range(5, -1, -1):
-        if j==0:
-            break
-        div(i, j)
